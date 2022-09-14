4119bf7a1faa155266819515f1430b96370751df
Sami Havuluoto <sami.havuluoto@gmail.com>
2022-09-14 16:57:59 +0100

    Add playRound function
    Added playRound function which simulates one round of rock, paper,
    scissors by using an if statement to compare the playerChoice and
    computerChoice variables.

--- script.js
+++ script.js
@@ -17,3 +17,27 @@ function getComputerChoice() {
 
 let computerChoice = getComputerChoice();
 
+function playRound(playerChoice, computerChoice){
+    if (playerChoice === "rock" && computerChoice === "scissors"){
+        return "You win! Rock beats scissors.";
+    }
+    else if(playerChoice === "paper" && computerChoice === "rock"){
+        return "You win! Paper beats rock.";
+    }
+    else if(playerChoice === "scissors" && computerChoice === "paper"){
+        return "You win! Scissors beats paper.";
+    }
+    else if(playerChoice === "rock" && computerChoice === "paper"){
+        return "You lose! Paper beats rock.";
+    }
+    else if(playerChoice === "paper" && computerChoice === "scissors"){
+        return "You lose! Scissors beat paper.";
+    }
+    else if(playerChoice === "scissors" && computerChoice === "rock"){
+        return "You lose! Rock beats scissors.";
+    }
+    else{
+        return "Invalid choice. Please choose rock, paper, or scissors."
+    }
+}
+
