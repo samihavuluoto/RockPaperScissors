299b4b60fa8a804f5e6bd5e46a19968373c9eca0
Sami Havuluoto <sami.havuluoto@gmail.com>
2022-12-06 18:04:53 +0000

    Add win counter and text
    Added counter to keep track of player and computers wins,
    which resets after 5 wins. Winning alert displayed at 5
    wins.

--- script.js
+++ script.js
@@ -20,37 +20,82 @@ function getComputerChoice() {
 function playRound(playerChoice, computerChoice){
 
     if (playerChoice === "rock" && computerChoice === "scissors"){
+        if(playerScore === 5){
+            playerScore = 0;
+        }
         playerScore += 1;
-        return "You win! Rock beats scissors.";
+        if(playerScore === 5){
+            alert(`The Player wins the match! ${playerScore} - ${computerScore}`);
+        }
+        return `You win! Rock beats scissors.
+        Player score: ${playerScore} Computer Score: ${computerScore}`;
     }
     else if(playerChoice === "paper" && computerChoice === "rock"){
+        if(playerScore === 5){
+            playerScore = 0;
+        }
         playerScore += 1;
-        return "You win! Paper beats rock.";
+        if(playerScore === 5){
+            alert(`The Player wins the match! ${playerScore} - ${computerScore}`);
+        }
+        return `You win! Paper beats rock.
+        Player score: ${playerScore} Computer Score: ${computerScore}`;
     }
     else if(playerChoice === "scissors" && computerChoice === "paper"){
+        if(playerScore === 5){
+            playerScore = 0;
+        }
         playerScore += 1;
-        return "You win! Scissors beats paper.";
+        if(playerScore === 5){
+            alert(`The Player wins the match! ${playerScore} - ${computerScore}`);
+        }
+        return `You win! Scissors beats paper.
+        Player score: ${playerScore} Computer Score: ${computerScore}`;
     }
     else if(playerChoice === "rock" && computerChoice === "paper"){
-        computerScore += 1;
-        return "You lose! Paper beats rock.";
+        if(computerScore === 5){
+            computerScore = 0;
+        }
+        computerScore+= 1;
+        if(computerScore === 5){
+            alert(`The Computer wins the match! ${computerScore} - ${playerScore}`);
+        }
+        return `You lose! Paper beats rock.
+        Player score: ${playerScore} Computer Score: ${computerScore}`;
     }
     else if(playerChoice === "paper" && computerChoice === "scissors"){
-        computerScore += 1;
-        return "You lose! Scissors beat paper.";
+        if(computerScore === 5){
+            computerScore = 0;
+        }
+        computerScore+= 1;
+        if(computerScore === 5){
+            alert(`The Computer wins the match! ${computerScore} - ${playerScore}`);
+        }
+        return `You lose! Scissors beat paper.
+        Player score: ${playerScore} Computer Score: ${computerScore}`;
     }
     else if(playerChoice === "scissors" && computerChoice === "rock"){
-        computerScore += 1;
-        return "You lose! Rock beats scissors.";
+        if(computerScore === 5){
+            computerScore = 0;
+        }
+        computerScore+= 1;
+        if(computerScore === 5){
+            alert(`The Computer wins the match! ${computerScore} - ${playerScore}`);
+        }
+        return `You lose! Rock beats scissors.
+        Player score: ${playerScore} Computer Score: ${computerScore}`;
     }
     else if(playerChoice === "scissors" && computerChoice === "scissors"){
-        return "It's a tie! You both chose scissors.";
+        return `It's a tie! You both chose scissors.
+        Player score: ${playerScore} Computer Score: ${computerScore}`;
     }
     else if(playerChoice === "rock" && computerChoice === "rock"){
-        return "It's a tie! You both chose rock.";
+        return `It's a tie! You both chose rock.
+        Player score: ${playerScore} Computer Score: ${computerScore}`;
     }
     else if(playerChoice === "paper" && computerChoice === "paper"){
-        return "It's a tie! You both chose paper.";
+        return `It's a tie! You both chose paper.
+        Player score: ${playerScore} Computer Score: ${computerScore}`;
     }
     else{
         return "Invalid choice. Please choose rock, paper, or scissors.";
